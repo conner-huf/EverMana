b47e7c1131f3b64c7b4cb4dbff5d56b95e847a12
Conner Hufnagel <conner.huf@gmail.com>
2024-02-23 17:52:33 -0800

    updates
    made the mana bar turn red when you can't shift into cat form and display you're current mana value

--- EverMana.lua
+++ EverMana.lua
@@ -1,10 +1,7 @@
 local frame = CreateFrame("StatusBar", "ManaBar", PlayerFrame)
 frame:SetSize(100, 9)  -- width, height
-
-frame:SetPoint("TOP", PlayerFrame, "BOTTOM", 50, 35)
-
+frame:SetPoint("TOP", PlayerFrame, "BOTTOM", 50, 33)
 frame:SetStatusBarTexture("Interface\\TARGETINGFRAME\\UI-StatusBar")
-
 frame:SetStatusBarColor(0, 0, 1)
 
 local bg = frame:CreateTexture(nil, "BACKGROUND")
@@ -17,13 +14,53 @@ frame:SetMinMaxValues(0, maxMana)
 local currentMana = UnitPower("player", 0)
 frame:SetValue(currentMana)
 
-frame:SetScript("OnUpdate", function(self, elapsed)
+local valueText = frame:CreateFontString(nil, "OVERLAY")
+valueText:SetFont("Fonts\\FRIZQT__.TTF", 10, "OUTLINE")
+valueText:SetPoint("CENTER", frame, "CENTER", 0, 0)
+
+local updateFrame = CreateFrame("Frame")
+updateFrame:Hide()
+
+updateFrame:SetScript("OnUpdate", function(self, elapsed)
     self.timeSinceLastUpdate = (self.timeSinceLastUpdate or 0) + elapsed
 
     if self.timeSinceLastUpdate >= 1 then  -- in seconds
         local currentMana = UnitPower("player", 0)
-        self:SetValue(currentMana)
+        frame:SetValue(currentMana)
+        valueText:SetText(currentMana)
+
         self.timeSinceLastUpdate = 0
+
+        local spellName = "Cat Form"
+        local spellId = select(7, GetSpellInfo(spellName))
+        local costInfo = GetSpellPowerCost(spellId)
+
+        if currentMana < costInfo[1].cost then  -- Change 100 to your desired value
+            frame:SetStatusBarColor(1, 0, 0)  -- Red
+        else
+            frame:SetStatusBarColor(0, 0, 1)  -- Blue
+        end
+
+        local hasAura = false
+        for i = 1, 40 do
+            local name = UnitAura("player", i)
+            if name == "Cat Form" or name == "Bear Form" then
+                hasAura = true
+                break
+            end
+        end
+
+        if hasAura then
+            -- If the player has the aura, show the mana bar
+            frame:Show()
+            -- print("Cat Form detected, showing mana bar.")
+        else
+            -- If the player doesn't have the aura, hide the mana bar
+            frame:Hide()
+            -- print("Cat Form not detected, hiding mana bar.")
+        end
 
     end
 end)
+
+updateFrame:Show()
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # EverMana
+
+- Update:
+I added some conditionals to make it disappear when you're not in cat form or bear form. It also turns red when you wouldn't have enough mana to shift into cat form and displays your current mana value.
+
  A super-lightweight WoW addon made to display a druid's mana level for powershifting
 
 I figure there are probably addons that do this, but they typically come with way too many other features. This just displays a little blue mana bar under your player frame no matter what form you're in. Useful for powershifting and not running out of mana to switch back into cat form.
